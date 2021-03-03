b68ff91f5729331f119772ab28de69c5480b0057
Mario Jorge Pereira <mariojp@gmail.com>
2021-03-03 07:59:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function bomdia(){
 		var h1v = document.querySelector("h1");
 		h1v.textContent = "UCSAL";
 }
+
+
+function botaoEvento() {
+	var senha = document.querySelector("#senha");
+		
+	if(senha.length < 6){
+		alert('Senha precisa ser maior que 6');	
+	}
+	alert('Botão clicado');
+}
